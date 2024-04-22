992628754113cfd53a4a7c583bfeb12a7b6f8d3e
jadizinhasl <jadi6486@gmail.com>
2024-04-22 01:39:03 -0300

    aula 01

---
+++ Aulas/Aula01/aula01.md
@@ -0,0 +1,24 @@
+#   Instalar Plugins
+    - Live server(HTML)
+    - Material Icon Theme
+
+
+### Extensão de arquivos
+    Tudo o que vem depois do "."
+    Exemplo:
+        .html
+        .js
+        .css
+        .cs
+        .c
+        .cpp
+        .java
+        .mp3
+        .map - .mov
+        .pdf
+        .docx
+        .xlsx
+
+### Executar um arquivo javascript com node
+    - Entrar na pasta do arquivo .js    | cd nome-da-pasta
+    - Executar o arquivo .js            | node nome-do-arquivo.js
---
+++ Aulas/Aula01/js/variaveis.js
@@ -0,0 +1,20 @@
+var nome = "Beltrano" // String
+var nome = String("Ciclano") // String
+
+var numero1 = 5 // Number
+var numero2 = 5.5   // Number
+var numero3 = Number("10")  // Number
+var numero4 = Number(100)   // Number
+var numero5 = parseInt(5.3) // Number
+var numero6 = parseFloat(10.5)  // Number
+
+var ehMaiorDeIdade = true   // boolean
+var ehVermelho = false // boolean
+
+var naoDefinido = undefined
+var vazio = null
+var naoEhNumero = NaN
+
+console.log(naoDefinido)
+console.log(vazio)
+console.log(naoEhNumero)
