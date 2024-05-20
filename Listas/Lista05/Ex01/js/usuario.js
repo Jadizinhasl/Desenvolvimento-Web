8c15d4527488f76340a77c9859fde4a6fa6bf650
jadizinhasl <jadi6486@gmail.com>
2024-05-20 14:47:34 -0300

    Primeiros passos com JS

---
+++ Listas/Lista05/Ex01/js/usuario.js
@@ -0,0 +1,6 @@
+var nome = prompt('Digite seu nome: ')
+var idade = Number(prompt('Digite sua idade: '))
+var emprego = prompt('Digite com o que você trabalha: ')
+var endereco = prompt('Digite seu endereço completo: ')
+
+alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${emprego} \nMora no endereço: ${endereco}`)
---
+++ Listas/Lista05/Ex02/ex02.js
@@ -0,0 +1,7 @@
+var nome = "Fulano"
+var idade = 19
+var emprego = "Analista de sistemas"
+var endereco = "Rua XPTO"
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${emprego} \nMora no endereço: ${endereco}`)
+
