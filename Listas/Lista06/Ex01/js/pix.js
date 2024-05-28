3d5d39c27e5e20f27e5b5c5a50579ee320b282fb
jadizinhasl <jadi6486@gmail.com>
2024-05-28 20:08:55 -0300

    Lista 6: Arrays e Objetos em Javascript

---
+++ Listas/Lista06/Ex01/js/pix.js
@@ -0,0 +1,28 @@
+var pix = {
+    chavePix: "amsbdbiodss-jbsdhisn-nxg8sdi",
+    valor: 194.32,
+    data: "21/04/2024",
+    descricaoPagamento: "Pagamento controle de Xbox",
+    pagador: {
+        nome: "Clenilson",
+        documento: 36209865432
+    },
+    recebedor: {
+        nome: "Jadizinhasl",
+        documento: 65498723409
+    },
+    transacoes: [
+        {
+            data: "21/04/2024",
+            valor: 194.32,
+            descricao: "Pagamento controle Xbox"
+        },
+        {
+            data: "04/05/2024",
+            valor: 75.00,
+            descricao: "Pagamento manutenção do aparelho"
+        }
+    ]
+}
+
+console.log(pix)
